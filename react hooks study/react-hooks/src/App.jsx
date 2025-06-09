b57bc8ed5abc597f23d19999cf9ed2f5f03adfcd
Avnish Srivastava <srivastavavicks@gmail.com>
2025-06-10 00:37:52 +0530

    callback and memo hook

--- react hooks study/react-hooks/src/App.jsx
+++ react hooks study/react-hooks/src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react'
+import { useState,useMemo,useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import { countercontext } from './context/context'
 
+
+const nums = new Array(30_000_000).fill(0).map((_,i)=>{
+  return{
+    index:i,
+    isMagical:i===29_000_000
+  }
+})
+
+
 function App() {
   const [count, setCount] = useState(0)
-
+  const getadjective = useCallback(()=>{
+  return "hello!" + count
+  },[count])
+  // const num = nums.find(item=>item.isMagical==true)//bht jyada expensive computation hoga kyuki har baar jb page render hoga toh ye rn krega jise react app lag krega
+  const [number, setnumber] = useState(nums)
+  const num = useMemo(() => number.find(item=>item.isMagical==true), [number])//isme usememo se "meoise" ho jyga ar srf ek baar chlega jb number change ho 
   return (
     <>
+    <div>Magical number is : {num.index}</div>
     <countercontext.Provider value={{count,setCount}}>
-    <Navbar/>
+    <Navbar adjective={"you are good"} getadjective={getadjective}/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -22,7 +37,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => {setCount((count) => count + 1); 
+        if(count===15){ setnumber(new Array(10_000_000).fill(0).map((_,i)=>{
+                  return{
+                  index:i,
+                  isMagical:i===9_000_000
+                  }
+                  }))
+                }
+            }}>
           count is {count}
         </button>
         <p>
--- react hooks study/react-hooks/src/components/Navbar.jsx
+++ react hooks study/react-hooks/src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React,{memo} from 'react'
 import Component1 from './Component1'
 
-const Navbar = () => {
+const Navbar = ({adjective,getadjective}) => {
+  console.log("hellow world")
   return (
     <>
     <div>
-      navbar
+      i am good and {adjective}
+      <button onClick={()=>{getadjective()}}>
+           {getadjective()}
+      </button>
     </div>
     <Component1/>
     </>
   )
 }
 
-export default Navbar
+export default memo(Navbar)
